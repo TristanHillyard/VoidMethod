8a3fb031453426a001ad7cc3705a91cf1b021e44
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2020-12-31 19:31:19 -0700

    Add project files.

---
+++ VoidMethod/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            newVoid math = new newVoid();
+            int num = 10;
+            int num2 = 5;
+            math.NewVoid(5, 10);
+            Console.WriteLine(math.NewVoid(num + num2));
+        }
+    }
+}
---
+++ VoidMethod/newVoid.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoidMethod
+{
+    public class newVoid
+    {
+        public void NewVoid(int num1, int num2)
+        {
+            int result;
+            int result2;
+            result2 = num2;
+            result = num1 + 2;
+            return;
+        }
+    }
+}
